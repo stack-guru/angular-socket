2ff646c0b9af84018f7c2471dba4516801949bbd
oliverchang <OliverChang0302@gmail.com>
2020-04-21 07:58:33 +1000

    add return type to service method

--- src/app/socket-data.service.ts
+++ src/app/socket-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject, from } from 'rxjs';
+import { Observable } from 'rxjs';
 import * as socketio from 'socket.io-client';
 
 @Injectable({
@@ -8,7 +9,7 @@ import * as socketio from 'socket.io-client';
 export class SocketDataService {
   private baseUrl = 'http://localhost:4001';
 
-  getData() {
+  getData(): Observable<number[]> {
     const socket = socketio(this.baseUrl);
     const data = new Subject<number[]>();
     const dataObservable = from(data);
